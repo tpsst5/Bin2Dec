5aa0f78fbf0a0a6ab8147985e296484a4a41f1e0
Tim Shea <tshea31@gmail.com>
2020-06-07 12:24:50 -0400

    Updated invalid number alert

--- app.js
+++ app.js
@@ -15,20 +15,28 @@ const decimalResult = document.getElementById('decimal-output-display');
 
 // Calculate/convert to decimal number
 function calculateDecimal() {
+  // Toggle var to track if valid input or not
+  let validInput = true;
+
   // Vars for input and output
   let binaryNum = binaryInput.value;
   const exponentNum = binaryNum.length - 1;
   let decimalNum = 0;
 
   // Check if nums entered contain invalid nums
-  if (binaryNum.match(/[2-9]/g)) {
+  if (binaryNum.match(/[2-9|a-z]/ig) || isNaN(parseInt(binaryNum))) {
+    binaryInput.value = '';
+    validInput = false;
     alert('Please enter either 1 or 0');
   }
 
-  // Convert binary input to decimal
-  for (let i = 0; i < binaryNum.length; i++) {
-    decimalNum += parseInt(binaryNum[i]) * Math.pow(2, exponentNum - i);
-  }
+  if (validInput === true) {
+    // Convert binary input to decimal
+    for (let i = 0; i < binaryNum.length; i++) {
+      decimalNum += parseInt(binaryNum[i]) * Math.pow(2, exponentNum - i);
+    }
 
-  decimalResult.value = decimalNum;
+    // Display decimal result
+    decimalResult.value = decimalNum;
+  }
 }
